6e79017219141b690251275f36f45f6ab0ecad8a
Serdar Halac <serdarhalax@gmail.com>
2019-07-06 17:54:00 -0600

    added basic html

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
-
 var app = express();
+var http = require('http').createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("ChatKat server is up!");
