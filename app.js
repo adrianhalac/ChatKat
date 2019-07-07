ad0caca9b408c72c9e6f2334bd97fc36b5f90a9d
Serdar Halac <serdarhalax@gmail.com>
2019-07-06 19:09:52 -0600

    add socket input form functionality

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
     console.log('A user has connected');
-
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg)
+    });
     socket.on('disconnect', function() {
         console.log('A user has disconnected');
     });
