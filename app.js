705530ae8b0c6febd9df1ec6529aa8527ff69d51
Serdar Halac <serdarhalax@gmail.com>
2019-07-07 12:19:01 -0600

    add message submit and display + scroll html box

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket) {
     console.log('A user has connected');
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg)
+        io.emit('chat message', msg);
     });
     socket.on('disconnect', function() {
         console.log('A user has disconnected');
