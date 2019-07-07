386cd34d72969b5f5dc24fdb75d7e79b1fdc59fc
Serdar Halac <serdarhalax@gmail.com>
2019-07-06 18:25:11 -0600

    add basic socket.io listening

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public/views');
@@ -11,8 +12,14 @@ app.get('/', function(req, res){
     res.render('index.html');
 });
 
+io.on('connection', function(socket) {
+    console.log('A user has connected');
 
+    socket.on('disconnect', function() {
+        console.log('A user has disconnected');
+    });
+});
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+http.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("ChatKat server is up!");
 });
